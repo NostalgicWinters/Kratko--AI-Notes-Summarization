80d68c3eb1c65aec7101f850678518ba8334eb7d
Dev <Dev.Sterling@outlook.com>
2026-01-09 20:03:52 +0530

    Started work

---
+++ kratko-frontend/src/pages/Header.jsx
@@ -0,0 +1,12 @@
+function Header()
+{
+    return(
+        <div className="flex flex-row justify-between bg-amber-100 p-4">
+            <h1 className="text-black text-3xl">Kratko</h1>
+            <h1 className="text-black text-3xl">Summarizer</h1>
+            <h1 className="text-black text-3xl">Profile</h1>
+        </div>
+    )
+}
+
+export default Header;
